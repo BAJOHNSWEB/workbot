a92f1845026cfe6834c59fb593e0ce26f3641b8c
Ben Johns <50174377+BAJOHNSWEB@users.noreply.github.com>
2025-11-30 23:57:00 +0000

    Update arthur-ai-content-assistant.php

--- arthur-ai-content-assistant/arthur-ai-content-assistant.php
+++ arthur-ai-content-assistant/arthur-ai-content-assistant.php
@@ -22,7 +22,7 @@ require_once ARTHUR_AI_PLUGIN_DIR . 'includes/class-arthur-ai-modules.php';
 require_once ARTHUR_AI_PLUGIN_DIR . 'includes/class-arthur-ai-service.php';
 require_once ARTHUR_AI_PLUGIN_DIR . 'includes/class-arthur-ai-rest.php';
 require_once ARTHUR_AI_PLUGIN_DIR . 'includes/class-arthur-ai-frontend.php';
-require_once ARTHUR_AI_PLUGIN_DIR . 'includes/modules/class-arthur-ai-login-customiser.php';
+
 require_once ARTHUR_AI_PLUGIN_DIR . 'includes/class-arthur-ai-login-customiser.php';
 
 
